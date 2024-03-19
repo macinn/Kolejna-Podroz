d0ef247eba066c4cfaea974b0a36b648be285524
lukaszszvmczvk <128000226+lukaszszvmczvk@users.noreply.github.com>
2024-03-19 19:12:25 +0100

    Lukasz/IO2-4 szkielety klas (#7)
    * create models
    
    * fix
    
    ---------
    
    Co-authored-by: macinn <118574079+macinn@users.noreply.github.com>

---
+++ KolejnaPodroz.Domain/Models/AdminSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolejnaPodroz.Domain.Models
+{
+    public class AdminSession
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Admin Admin { get; set; }
+    }
+}
---
+++ KolejnaPodroz.Domain/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolejnaPodroz.Domain.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public Connection Connection { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        public User User { get; set; }
+    }
+}
