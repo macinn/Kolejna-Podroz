f42189126e54a87429ab7dc8e14dff9a8c75444f
marcinn <01171345@pw.edu.pl>
2024-03-19 18:51:56 +0100

    enable cors

--- KolejnaPodrozApp/Program.cs
+++ KolejnaPodrozApp/Program.cs
@@ -1,19 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var PolicyName = "AllowAll";
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(PolicyName,
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseCors(PolicyName);
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
