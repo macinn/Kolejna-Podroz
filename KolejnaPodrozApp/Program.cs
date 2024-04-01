d31e4bb394229d847f90ed087935e6ba5b422852
marcinn <01171345@pw.edu.pl>
2024-04-01 21:44:12 +0200

    swagger as default page

--- KolejnaPodrozApp/Program.cs
+++ KolejnaPodrozApp/Program.cs
@@ -2,6 +2,7 @@ using KolejnaPodroz.DataAccess.Data;
 using KolejnaPodroz.DataAccess.Repository.IRepository;
 using KolejnaPodroz.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,7 +40,11 @@ var app = builder.Build();
 //{
 app.UseCors(PolicyName);
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kolejna Podroz API V1");
+    c.RoutePrefix = "";
+});
 //}
 
 app.UseHttpsRedirection();
