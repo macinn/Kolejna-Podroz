deafa75ced14da9212483a503b06df11f3214bb8
marcinn <01171345@pw.edu.pl>
2024-04-02 20:20:37 +0200

    vs react+asp proj

--- KolejnaPodrozApp/Program.cs
+++ KolejnaPodrozApp/Program.cs
@@ -1,48 +1,30 @@
-using KolejnaPodroz.DataAccess.Data;
-using KolejnaPodroz.DataAccess.Repository.IRepository;
-using KolejnaPodroz.DataAccess.Repository;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Rewrite;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-var PolicyName = "AllowAll";
-// Add services to the container.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(PolicyName,
-        builder =>
-        {
-            builder
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-        });
-});
-
 var app = builder.Build();
 
-app.UseCors(PolicyName);
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
 
 app.Run();
