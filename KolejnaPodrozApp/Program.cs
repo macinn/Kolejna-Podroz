7284b6893d34ef1dadc130aa565361cdec2ff9d5
marcinn <01171345@pw.edu.pl>
2024-04-02 12:46:40 +0200

    swagger removed from homepage

--- KolejnaPodrozApp/Program.cs
+++ KolejnaPodrozApp/Program.cs
@@ -35,17 +35,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
 app.UseCors(PolicyName);
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kolejna Podroz API V1");
-    c.RoutePrefix = "";
-});
-//}
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
