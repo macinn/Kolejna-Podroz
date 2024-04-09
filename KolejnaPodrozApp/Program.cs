4223491063a37ed2a874f9f031d11e4fcaf3c615
macinn <118574079+macinn@users.noreply.github.com>
2024-04-09 20:10:34 +0200

    Marcin/io2 125 react workflow (#22)
    * setup for react app
    
    * test workflow
    
    * node version fix
    
    * build job body
    
    * cache dep path
    
    * node version change
    
    * s
    
    * quotes
    
    * debug ls
    
    * node version update
    
    * cleanup
    
    * remove debug branch

--- KolejnaPodrozApp/Program.cs
+++ KolejnaPodrozApp/Program.cs
@@ -40,12 +40,6 @@ app.UseDefaultFiles();
 
 app.UseCors(PolicyName);
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kolejna Podroz API V1");
-    c.RoutePrefix = "";
-});
-//}
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
