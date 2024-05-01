5afef344aac9d3b759a9ab0e9bb176e96bca3bb9
barsew <131254106+barsew@users.noreply.github.com>
2024-05-01 12:55:30 +0200

    Dodanie emailservice (#36)
    Co-authored-by: Bartosz Seweryn <01171342@pw.edu.pl>

---
+++ KolejnaPodroz.Domain/Services/EmailService/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolejnaPodroz.Domain.Services.EmailService
+{
+    public interface IEmailService
+    {
+        public Task SendEmail(string subject, string toEmail, string username, string message);
+    }
+}
