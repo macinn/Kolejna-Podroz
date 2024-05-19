642e63a564fe4950858b7927d2bd60163a6e41a6
barsew <bartosz.seweryn@wp.pl>
2024-05-19 23:19:41 +0200

    get tickets i user controller

--- KolejnaPodroz.Test/TicketControllerTest.cs
+++ KolejnaPodroz.Test/TicketControllerTest.cs
@@ -1,21 +1,46 @@
-﻿using KolejnaPodroz.DataAccess.Repository.IRepository;
+﻿using KolejnaPodroz.DataAccess.Repository;
+using KolejnaPodroz.DataAccess.Repository.IRepository;
+using KolejnaPodroz.Domain.Models;
 using KolejnaPodroz.Domain.Services.EmailService;
 using KolejnaPodroz.Domain.Services.TicketService;
 using KolejnaPodrozApp.Controllers;
+using KolejnaPodrozApp.Models.Station;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
+using MySqlX.XDevAPI.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace KolejnaPodroz.Test
 {
     public class TicketControllerTest
     {
-        private static readonly Mock<IUnitOfWork> UnitOfWorkMock = new Mock<IUnitOfWork>();
+        private static readonly Mock<IUnitOfWork> _unitOfWork = new Mock<IUnitOfWork>();
         private static readonly ITicketService _ticketService = new TicketService();
         private static readonly IEmailService _emailService = new EmailService();
-        private readonly TicketController _controller = new TicketController(UnitOfWorkMock.Object, _ticketService,_emailService);
+        private readonly TicketController _controller = new TicketController(_unitOfWork.Object, _ticketService,_emailService);
+
+        [Fact]
+        public void GetByUserId_ReturnsZeroTickets()
+        {
+            // Arrange
+            string auth0id = "invalid";
+            var controller = new TicketController(_unitOfWork.Object, _ticketService, _emailService);
+            var tickets = new List<Ticket>();
+            _unitOfWork.Setup(u => u.Ticket.GetAll(It.IsAny<Expression<Func<Ticket, bool>>>())).Returns(tickets);
+
+            // Act
+            var response = controller.Get(auth0id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(response.Result);
+            var returnTickets = Assert.IsType<List<Ticket>>(okResult.Value);
+            Assert.Empty(returnTickets);
+        }
     }
 }
---
+++ KolejnaPodrozApp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using KolejnaPodroz.DataAccess.Repository.IRepository;
+using KolejnaPodroz.Domain.Models;
+using KolejnaPodroz.Domain.Services.EmailService;
+using KolejnaPodroz.Domain.Services.TicketService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KolejnaPodrozApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public ActionResult<AccountInfo> Get(string auth0Id)
+        {
+            var user = _unitOfWork.User.Get(u => u.Auth0Id == auth0Id);
+            return Ok(user.AccountInfo);
+
+        }
+    }
+}
