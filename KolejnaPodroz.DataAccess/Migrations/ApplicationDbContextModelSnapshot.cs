5a9ea2bd3acb04fe224ff1f3803289cf6638505a
marcinn <01171345@pw.edu.pl>
2024-05-20 02:02:55 +0200

    nowe db

--- KolejnaPodroz.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ KolejnaPodroz.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace KolejnaPodroz.DataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.4")
+                .HasAnnotation("ProductVersion", "8.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("KolejnaPodroz.Domain.Models.AccountInfo", b =>
